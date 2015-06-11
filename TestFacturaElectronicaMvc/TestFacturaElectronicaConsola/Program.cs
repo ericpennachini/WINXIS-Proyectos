cccbdd0fc20a9b0b3dec496da498641a4092d235
Eric Pennachini <eric.pennachini@gmail.com>
2015-06-11 11:51:43 -0300

    TRABAJANDO CON WS
    Se hizo un cambio para trabajar directamente con el WS, sin usar un OCX
    como antes. [en proceso]

--- TestFacturaElectronicaMvc/TestFacturaElectronicaConsola/Program.cs
+++ TestFacturaElectronicaMvc/TestFacturaElectronicaConsola/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using TestFacturaElectronicaDominio;
 
 namespace TestFacturaElectronicaConsola
 {
@@ -9,6 +10,11 @@ namespace TestFacturaElectronicaConsola
     {
         static void Main(string[] args)
         {
+            TestFacturaElectronicaDominio.FacturaElectronicaWS.ServiceSoapClient f = new TestFacturaElectronicaDominio.FacturaElectronicaWS.ServiceSoapClient();
+            TestFacturaElectronicaDominio.FacturaElectronicaWS.FECAERequest request = new TestFacturaElectronicaDominio.FacturaElectronicaWS.FECAERequest();
+            //request.FeCabReq.
+            TestFacturaElectronicaDominio.FacturaElectronicaWS.FECAEResponse response = new TestFacturaElectronicaDominio.FacturaElectronicaWS.FECAEResponse();
+            response = f.FECAESolicitar(
         }
     }
 }
--- TestFacturaElectronicaMvc/TestFacturaElectronicaDominio/ServFactElect.cs
+++ TestFacturaElectronicaMvc/TestFacturaElectronicaDominio/ServFactElect.cs
@@ -2,29 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using WSAFIPFE;
 using TestFacturaElectronicaDominio.FacturaElectronicaWS;
 
 namespace TestFacturaElectronicaDominio
 {
-    /// <summary>
-    /// Ver este link:
-    /// https://sites.google.com/site/facturaelectronicax/Home/ejemplos/ejemplo-net
-    /// </summary>
     public class ServFactElect
     {
-
-        private Factura factura = new Factura();
-
-        public ServFactElect() { }
-
-        public string Facturar()
-        {
-            
-            
-            
-            return "";
-        }
-
+        
     }
 }
