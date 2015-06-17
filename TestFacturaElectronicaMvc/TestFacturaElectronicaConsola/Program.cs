f71f3671401f9bc0930caffbe8c59049a1674df9
Eric Pennachini <eric.pennachini@gmail.com>
2015-06-17 12:48:13 -0300

    PARCIALMENTE TERMINADO
    Se autentica al servidor de AFIP con una app, la cual devuelve token y
    sign. Se manda el request y devuelve el response, y se pueden mostrar
    los resultados. Devuelve CAE.
    
    FALTA AUTENTICARSE DESDE EL CODIGO

--- TestFacturaElectronicaMvc/TestFacturaElectronicaConsola/Program.cs
+++ TestFacturaElectronicaMvc/TestFacturaElectronicaConsola/Program.cs
@@ -12,30 +12,76 @@ namespace TestFacturaElectronicaConsola
     {
         static void Main(string[] args)
         {
-            //try
-            //{
-            
-            loginCmsRequest logReq = new loginCmsRequest();
-            logReq.in0 = "C:\\Program Files (x86)\\PyAfipWs\\pennachini_prueba_wsass.crt";
-            
+            try
+            {
                 
-                ServiceSoapClient f = new ServiceSoapClient();
+                ServiceSoapClient fServ = new ServiceSoapClient();
                 ServFactElect servicio = new ServFactElect();
                 FEAuthRequest autorizacion = new FEAuthRequest();
                 FECAERequest request = new FECAERequest();
                 FECAEResponse response = new FECAEResponse();
-                autorizacion = servicio.Autorizar();
-                request = servicio.ConfigurarRequest();
+                LoginCMSClient login = new LoginCMSClient(); // ver como autenticarse desde el codigo
+            
+                
+                //Instancio algunos campos del response que son compuestos, y tambien sus subcampos
+                //Observaciones (solo uno para prueba)
+                response.FeDetResp = new FECAEDetResponse[1];
+                response.FeDetResp[0] = new FECAEDetResponse();
+                response.FeDetResp[0].Observaciones = new Obs[1];
+                response.FeDetResp[0].Observaciones[0] = new Obs();
+                
+                //Errores
+                response.Errors = new Err[5];
+                for (int i = 0; i < 5; i++)
+                {
+                    response.Errors[i] = new Err();
+                }
 
-                response = f.FECAESolicitar(autorizacion, request); //necesita una autorizacion y el request
-            //}
-            //catch (Exception ex)
-            //{
-            //    Console.WriteLine("Ocurrio el siguiente error: \n\n" + ex.Message);
-            //    Console.WriteLine("\n \n- Tipo de excepcion: " + ex.GetType());
-            //    Console.WriteLine("\n \n- En: " + ex.TargetSite);
-            //    Console.ReadKey();
-            //}
+                
+                //Obtengo la autorizacion
+                autorizacion = servicio.Autorizar();
+                //Llamo al método que arma el request, que serian los campos de la factura
+                request = servicio.ConfigurarRequest();
+                //Llamo al web service, y me devuelve la respuesta (response)
+                response = fServ.FECAESolicitar(autorizacion, request); //necesita una autorizacion y el request
+
+                //Muestro los resultados
+                switch (response.FeCabResp.Resultado)
+                {
+                    case "A":
+                        Console.WriteLine("Resultado: " + response.FeCabResp.Resultado + " (Aprobado)");
+                        break;
+                    case "P":
+                        Console.WriteLine("Resultado: " + response.FeCabResp.Resultado + " (Parcial)");
+                        break;
+                    case "R":
+                        Console.WriteLine("Resultado: " + response.FeCabResp.Resultado + " (Rechazado)");
+                        break;
+                }
+
+                if (response.Errors != null)
+                {
+                    for (int i = 0; i < response.Errors.Length; i++)
+                    {
+                        Console.WriteLine("Error: " + response.Errors[i].Code + " -> " + response.Errors[i].Msg);
+                        Console.WriteLine("-------------");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("CAE = " + response.FeDetResp[0].CAE + "\n Vencimiento: " + servicio.ConvertirAFormatoFecha(response.FeDetResp[0].CAEFchVto));
+                    Console.WriteLine(response.FeCabResp.PtoVta);
+                }
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("····· EXCEPCION ·····");
+                Console.WriteLine("Ocurrio el siguiente error: \n\n" + ex.Message);
+                Console.WriteLine("\n \n- Tipo de excepcion: " + ex.GetType());
+                Console.WriteLine("\n \n- En: " + ex.TargetSite);
+                Console.ReadKey();
+            }
             
         }
     }
--- TestFacturaElectronicaMvc/TestFacturaElectronicaDominio/ServFactElect.cs
+++ TestFacturaElectronicaMvc/TestFacturaElectronicaDominio/ServFactElect.cs
@@ -9,69 +9,81 @@ namespace TestFacturaElectronicaDominio
 {
     public class ServFactElect
     {
-
+        //ServiceSoapClient fServ = new ServiceSoapClient();
+        
         public FEAuthRequest Autorizar()
         {
 
-            FEAuthRequest autorizacion = new FEAuthRequest();
+            FEAuthRequest _autorizacion = new FEAuthRequest();
 
-            autorizacion.Sign = "";
-            autorizacion.Token = "";
-            autorizacion.Cuit = 20360999301;
+            _autorizacion.Token = "PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/Pgo8c3NvIHZlcnNpb249IjIuMCI+CiAgICA8aWQgdW5pcXVlX2lkPSIxMzI3MTk1MDgwIiBzcmM9IkNOPXdzYWFob21vLCBPPUFGSVAsIEM9QVIsIFNFUklBTE5VTUJFUj1DVUlUIDMzNjkzNDUwMjM5IiBnZW5fdGltZT0iMTQzNDU0MTgyNiIgZXhwX3RpbWU9IjE0MzQ1ODUwODYiIGRzdD0iQ049d3NmZSwgTz1BRklQLCBDPUFSIi8+CiAgICA8b3BlcmF0aW9uIHZhbHVlPSJncmFudGVkIiB0eXBlPSJsb2dpbiI+CiAgICAgICAgPGxvZ2luIHVpZD0iQz1hciwgTz1wZW5uYWNoaW5pIGVyaWMgZGFuaWVsLCBTRVJJQUxOVU1CRVI9Q1VJVCAyMDM2MDk5OTMwMSwgQ049cHlhZmlwd3MiIHNlcnZpY2U9IndzZmUiIHJlZ21ldGhvZD0iMjIiIGVudGl0eT0iMzM2OTM0NTAyMzkiIGF1dGhtZXRob2Q9ImNtcyI+CiAgICAgICAgICAgIDxyZWxhdGlvbnM+CiAgICAgICAgICAgICAgICA8cmVsYXRpb24gcmVsdHlwZT0iNCIga2V5PSIyMDM2MDk5OTMwMSIvPgogICAgICAgICAgICA8L3JlbGF0aW9ucz4KICAgICAgICA8L2xvZ2luPgogICAgPC9vcGVyYXRpb24+Cjwvc3NvPgoK";
+            _autorizacion.Sign = "KJGlK4vxSxHxkbqLeOyaIUSVBk/MYBg5ouxIAsWWsZjqZljQNMvWvxSP2UFp2DBRmtiWO3U1sfcfBd1+xhFNa6uRLA84uc20b90wbZR5N8wy74pFEPHuhdhyug35k2Huzzwtpg+s501KRvhBWzG6b40/bfO8OdRQODcOo5BY/c4=";
+            _autorizacion.Cuit = 20360999301;
             
-            return autorizacion;
+            return _autorizacion;
 
         }
 
-
         public FECAERequest ConfigurarRequest()
         {
-            FECAERequest request = new TestFacturaElectronicaDominio.FacturaElectronicaWS.FECAERequest();
-            FECAECabRequest cabecera = new FECAECabRequest();
-            FECAEDetRequest detalle = new FECAEDetRequest();
-            
+            FECAERequest _request = new TestFacturaElectronicaDominio.FacturaElectronicaWS.FECAERequest();
+            FECAECabRequest _cabecera = new FECAECabRequest();
+            FECAEDetRequest _detalle = new FECAEDetRequest();
+
             //Cabecera
-            cabecera.CantReg = 1;
-            cabecera.PtoVta = 1;
-            cabecera.CbteTipo = 1; //factura "A"
+            _cabecera.CantReg = 1;
+            _cabecera.PtoVta = 1;
+            _cabecera.CbteTipo = 1; //factura "A"
             //Detalle - NO OBLIGATORIO = N
-            detalle.Concepto = 1; //Productos
-            detalle.DocTipo = 80; //CUIT
-            detalle.DocNro = 20360999301;
-            detalle.CbteDesde = 1;
-            detalle.CbteHasta = 1;
-            detalle.CbteFch = "20150612";
-            detalle.ImpTotal = 121;
-            detalle.ImpTotConc = 0;
-            detalle.ImpNeto = 100;
-            detalle.ImpIVA = 21;
-            detalle.ImpOpEx = 0;
-            detalle.ImpTrib = 0;
+            _detalle.Concepto = 1; //Productos
+            _detalle.DocTipo = 80; //CUIT
+            _detalle.DocNro = 20377033251;
+            _detalle.CbteDesde = 13;
+            _detalle.CbteHasta = 13;
+            _detalle.CbteFch = "20150617";
+            _detalle.ImpTotal = 121;
+            _detalle.ImpTotConc = 0;
+            _detalle.ImpNeto = 100;
+            _detalle.ImpIVA = 21;
+            _detalle.ImpOpEx = 0;
+            _detalle.ImpTrib = 0;
             //detalle.FchServDesde = "";  N
             //detalle.FchServHasta = "";  N
             //detalle.FchVtoPago = "";    N
-            //FEParamGetTiposMonedasRequest moneda = new FEParamGetTiposMonedasRequest(new FEParamGetTiposMonedasRequestBody());
-            detalle.MonId = "PES";
-            detalle.MonCotiz = 1;
-            detalle.CbtesAsoc = null;
-            detalle.Tributos = null;
-            //AlicIva Iva = new AlicIva();
-            //Iva.Id = 5;
-            //Iva.BaseImp = 100;
-            //Iva.Importe = 21;
-            detalle.Iva[0] = new AlicIva
+            _detalle.MonId = "PES";
+            _detalle.MonCotiz = 1;
+            _detalle.CbtesAsoc = null;
+            _detalle.Tributos = null;
+            _detalle.Iva = new AlicIva[1]; //solo uno a modo de ejemplo
+            _detalle.Iva[0] = new AlicIva
             {
                 Id = 5,
                 Importe = 21,
                 BaseImp = 100
             };
             
+
+            _request.FeCabReq = _cabecera;
+            _request.FeDetReq = new FECAEDetRequest[1];
+            _request.FeDetReq[0] = new FECAEDetRequest();
+            _request.FeDetReq[0] = _detalle;
+
+            return _request;
+        }
+
+        //public FECAEResponse Solicitar()
+        //{
+        //}    
+        
+        public DateTime ConvertirAFormatoFecha(string fechaComprobante)
+        {
+            string _dia, _mes, _anio;
+            _anio = fechaComprobante.Substring(0, 4);
+            _mes = fechaComprobante.Substring(4, 2);
+            _dia = fechaComprobante.Substring(6, 2);
+            DateTime _fechaADevolver = new DateTime(Convert.ToInt32(_anio), Convert.ToInt32(_mes), Convert.ToInt32(_dia));
             
-
-            request.FeCabReq = cabecera;
-            request.FeDetReq[0] = detalle;
-
-            return request;
+            return _fechaADevolver;
         }
     }
 }
